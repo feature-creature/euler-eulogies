840d4be909f06c7443c05294b1b5aa513dcfd8ad
Feature Creature <jdema003@gold.ac.uk>
2019-02-01 01:35:00 -0500

    init 0001

---
+++ ee_0001_multiples_of_three_and_five.py
@@ -0,0 +1,35 @@
+#! usr/bin/env python
+
+# author    : Luke Demarest
+# title     : Multiples of Three and Five
+# license   : GPLv3
+# date      : 2019-02-01
+
+# exercise  : 0001
+
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    """invoke the main processes of the program"""
+
+
+
+
+if __name__ == "__main__":
+    main()
