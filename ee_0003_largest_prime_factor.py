999b280fc33908d90624ba5f14fc876b50b5c0dc
Feature Creature <jdema003@gold.ac.uk>
2019-04-29 23:15:19 +0700

    init 0003

---
+++ ee_0003_largest_prime_factor.py
@@ -0,0 +1,31 @@
+#! usr/bin/env python
+
+# author    : Luke Demarest
+# title     : Largest Prime Factor
+# license   : GPLv3
+# date      : 2019-04-29
+
+# exercise  : 0003
+
+'''
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+'''
+
+n = 13195
+
+
+
+
+
+
+
+def main():
+    """Find the sum of all the multiples of 3 or 5 below the inputted value."""
+
+
+if __name__ == "__main__":
+    main()
